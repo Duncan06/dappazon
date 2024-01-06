f2f77398b946cf65c796d54f25f955160cff9a3c
duncan06 <tylerdownie0@gmail.com>
2024-01-06 13:14:32 -0800

    Start setting up project.

--- contracts/Dappazon.sol
+++ contracts/Dappazon.sol
@@ -1,4 +1,28 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract Dappazon {}
+contract Dappazon {
+    string public name = "Dappazon";
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    // List products
+    function list(
+        uint256 _id, 
+        string memory _name, 
+        string memory _category,
+        string memory _image, 
+        uint256 _cost,
+        uint256 _rating,
+        uint256 _stock
+        ) public {
+
+    }
+
+    // Buy products
+
+    // Withdraw funds
+}
